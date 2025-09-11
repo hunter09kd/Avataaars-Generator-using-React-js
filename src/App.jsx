4786b8c1ee53b6c495d12867f96e1a7bd6e80fb5
devhimelali <dev.himelali@gmail.com>
2025-09-12 00:55:29 +0600

    Improve responsive spacing and text handling in AvatarGenerator and App components

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
     return (
         <>
             <div
-                className="min-h-screen bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900 flex items-center justify-center text-white overflow-hidden animate__animated animate__fadeIn">
+                className="min-h-screen px-2 md:px-0 bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900 flex items-center justify-center text-white overflow-hidden animate__animated animate__fadeIn">
                 <AvatarGenerator/>
             </div>
             <ToastContainer/>
--- src/assets/components/AvatarGenerator.jsx
+++ src/assets/components/AvatarGenerator.jsx
@@ -37,7 +37,7 @@ export default function AvatarGenerator() {
 
     return (
         <div
-            className="w-full max-w-md rounded-2xl shadow-xl backdrop-blur-xl border border-slate-70 p-10 flex flex-col items-center gap-8 animate__animated animate__slideInUp animate__faster">
+            className="w-full max-w-md rounded-2xl shadow-xl backdrop-blur-xl border border-slate-70 p-4 md:p-10 flex flex-col items-center gap-8 animate__animated animate__slideInUp animate__faster">
             <img src={state.imageUrl} alt="Avatar Image"
                  className="w-32 h-32 rounded-full border-4 border-slate-700 shadow-lg object-cover"/>
             <div className="text-center">
@@ -49,11 +49,11 @@ export default function AvatarGenerator() {
                 <SelectOption options={state.options} value={state.selectedOption}
                               onOptionChange={(val) => dispatch({type: "SET_OPTION", payload: val})}/>
                 <div
-                    className="w-full bg-slate-900/60 text-white rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-slate-500">
+                    className="w-full bg-slate-900/60 text-white rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-slate-500 break-words break-all">
                     {state.imageUrl}
                 </div>
             </div>
-            <div className="w-full flex justify-center gap-4">
+            <div className="w-full flex justify-center gap-2 md:gap-4">
                 <ActionButton
                     label="Change"
                     icon="ri-arrow-right-up-line"
